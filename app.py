3a717ede874112240315a0444d4cbcf0e96b8c08
luccacastro <luccacastro1008@poli.ufrj.br>
2022-01-26 09:12:13 -0300

    Adicionando contador para disciplinas

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def mainWindow():
     nb.add(aba5, text="Disciplinas Pendentes")
 
     f = open("grade.txt", encoding="utf8")
-    disciplinasSelecionadas = []
+    selectedDisciplines = []
     grade = f.readlines()
 
     gradeLabel = Label(aba2, text="Grade Curricular: Engenharia de Controle e Automação", bd=2, font=25, borderwidth=2, relief="flat")
@@ -60,106 +60,140 @@ def mainWindow():
     def selecionarDisciplinas():
         status = mySpin.get()
 
-        disciplinasSelecionadas = []
+        selectedDisciplines = []
+        notSelectedDisciplines = []
+        currentTv = 0
+        currentLabel = 0
 
         if status == "Disciplinas Cursadas":
-            for row in tv2.get_children():
-                tv2.delete(row)
+            currentTv = takenTv
+            currentLabel = takenNumberLabel
+            for row in takenTv.get_children():
+                takenTv.delete(row)
             nb.select(aba3)
         elif status == "Disciplinas em Curso":
-            for row in tv3.get_children():
-                tv3.delete(row)
+            currentTv = inCourseTv
+            currentLabel = inCourseNumberLabel
+            for row in inCourseTv.get_children():
+                inCourseTv.delete(row)
             nb.select(aba4)
         elif status == "Disciplinas Pendentes":
-            for row in tv4.get_children():
-                tv4.delete(row)
+            currentTv = pendingTv
+            currentLabel = PendingNumberLabel
+            for row in pendingTv.get_children():
+                pendingTv.delete(row)
             nb.select(aba5)
-        
-        aux = tv.get_checked()
 
-        for i in aux:
-            disciplinasSelecionadas.append(tv.item(i))
+        checkedDisciplines = curriculumTv.get_checked()
 
-        for i in disciplinasSelecionadas:
+        for i in curriculumTv.get_children():
+            if i in checkedDisciplines:
+                selectedDisciplines.append(curriculumTv.item(i))
+            else:
+                notSelectedDisciplines.append(curriculumTv.item(i))
+            
+
+        for i in selectedDisciplines:
             if status == "Disciplinas Cursadas":
-                tv2.insert("", "end", values=i["values"])
+                takenTv.insert("", "end", values=i["values"])
             elif status == "Disciplinas em Curso":
-                tv3.insert("", "end", values=i["values"])
+                inCourseTv.insert("", "end", values=i["values"])
             elif status == "Disciplinas Pendentes":
-                tv4.insert("", "end", values=i["values"])
-        
+                pendingTv.insert("", "end", values=i["values"])
 
-    
+        for i in notSelectedDisciplines:
+            if status == "Disciplinas Cursadas":
+                pendingTv.insert("", "end", values=i["values"])
+                updateDisciplineNumber("Disciplinas Pendentes", pendingTv, PendingNumberLabel)
+            elif status == "Disciplinas Pendentes":
+                takenTv.insert("", "end", values=i["values"])
+                updateDisciplineNumber("Disciplinas Cursadas", takenTv, takenNumberLabel)
+
+
+        updateDisciplineNumber(status, currentTv, currentLabel)
+
+        
+    def updateDisciplineNumber(status, tv, label):
+        label["text"] = "Você possui " + str(len(tv.get_children())) + " " + status
+
 
     saveButton = Button(aba2, text ="Salvar Disciplinas Selecionadas", command = selecionarDisciplinas)
     saveButton.pack(pady=5)
     
-    tv = CheckboxTreeview(aba2, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings", "tree"), height=15)
-    tv.column("#0", width=45)
-    tv.column("Código",  width=60, anchor=CENTER, stretch=NO)
-    tv.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
-    tv.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
-    tv.column("C.H",  width=40, anchor=CENTER, stretch=NO)
-    tv.column("Período", width=60, anchor=CENTER, stretch=NO)
-    tv.heading("Código",  text="CÓDIGO")
-    tv.heading("Disciplina", text="DISCIPLINA")
-    tv.heading("Créditos",  text="CRÉDITOS")
-    tv.heading("C.H",  text="C.H")   
-    tv.heading("Período", text="PERÍODO")
-    tv.pack()
+    curriculumTv = CheckboxTreeview(aba2, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings", "tree"), height=15)
+    curriculumTv.column("#0", width=45)
+    curriculumTv.column("Código",  width=60, anchor=CENTER, stretch=NO)
+    curriculumTv.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
+    curriculumTv.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
+    curriculumTv.column("C.H",  width=40, anchor=CENTER, stretch=NO)
+    curriculumTv.column("Período", width=60, anchor=CENTER, stretch=NO)
+    curriculumTv.heading("Código",  text="CÓDIGO")
+    curriculumTv.heading("Disciplina", text="DISCIPLINA")
+    curriculumTv.heading("Créditos",  text="CRÉDITOS")
+    curriculumTv.heading("C.H",  text="C.H")   
+    curriculumTv.heading("Período", text="PERÍODO")
+    curriculumTv.pack()
 
-    cursadasLabel = Label(aba3, text="Disciplinas Cursadas", bd=2, font=25, borderwidth=2, relief="flat")
-    cursadasLabel.pack(pady=10)
+    takenLabel = Label(aba3, text="Disciplinas Cursadas", bd=2, font=25, borderwidth=2, relief="flat")
+    takenLabel.pack(pady=10)
 
-    tv2 = ttk.Treeview(aba3, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
-    tv2.column("Código",  width=60, anchor=CENTER, stretch=NO)
-    tv2.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
-    tv2.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
-    tv2.column("C.H",  width=40, anchor=CENTER, stretch=NO)
-    tv2.column("Período", width=60, anchor=CENTER, stretch=NO)
-    tv2.heading("Código",  text="CÓDIGO")
-    tv2.heading("Disciplina", text="DISCIPLINA")
-    tv2.heading("Créditos",  text="CRÉDITOS")
-    tv2.heading("C.H",  text="C.H")   
-    tv2.heading("Período", text="PERÍODO")
-    tv2.pack()
+    takenTv = ttk.Treeview(aba3, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
+    takenTv.column("Código",  width=60, anchor=CENTER, stretch=NO)
+    takenTv.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
+    takenTv.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
+    takenTv.column("C.H",  width=40, anchor=CENTER, stretch=NO)
+    takenTv.column("Período", width=60, anchor=CENTER, stretch=NO)
+    takenTv.heading("Código",  text="CÓDIGO")
+    takenTv.heading("Disciplina", text="DISCIPLINA")
+    takenTv.heading("Créditos",  text="CRÉDITOS")
+    takenTv.heading("C.H",  text="C.H")   
+    takenTv.heading("Período", text="PERÍODO")
+    takenTv.pack()
 
-    emCursoLabel = Label(aba4, text="Disciplinas Em Curso", bd=2, font=25, borderwidth=2, relief="flat")
-    emCursoLabel.pack(pady=10)
+    takenNumberLabel = Label(aba3, text="Você possui " + str(len(takenTv.get_children())) + " Disciplinas em cursadas", bd=2, font=25, borderwidth=2, relief="flat")
+    takenNumberLabel.pack(pady=10)
 
-    tv3 = ttk.Treeview(aba4, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
-    tv3.column("Código",  width=60, anchor=CENTER, stretch=NO)
-    tv3.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
-    tv3.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
-    tv3.column("C.H",  width=40, anchor=CENTER, stretch=NO)
-    tv3.column("Período", width=60, anchor=CENTER, stretch=NO)
-    tv3.heading("Código",  text="CÓDIGO")
-    tv3.heading("Disciplina", text="DISCIPLINA")
-    tv3.heading("Créditos",  text="CRÉDITOS")
-    tv3.heading("C.H",  text="C.H")   
-    tv3.heading("Período", text="PERÍODO")
-    tv3.pack()
+    inCourseLabel = Label(aba4, text="Disciplinas Em Curso", bd=2, font=25, borderwidth=2, relief="flat")
+    inCourseLabel.pack(pady=10)
 
-    pendentesLabel = Label(aba5, text="Disciplinas Pendentes", bd=2, font=25, borderwidth=2, relief="flat")
-    pendentesLabel.pack(pady=10)
+    inCourseTv = ttk.Treeview(aba4, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
+    inCourseTv.column("Código",  width=60, anchor=CENTER, stretch=NO)
+    inCourseTv.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
+    inCourseTv.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
+    inCourseTv.column("C.H",  width=40, anchor=CENTER, stretch=NO)
+    inCourseTv.column("Período", width=60, anchor=CENTER, stretch=NO)
+    inCourseTv.heading("Código",  text="CÓDIGO")
+    inCourseTv.heading("Disciplina", text="DISCIPLINA")
+    inCourseTv.heading("Créditos",  text="CRÉDITOS")
+    inCourseTv.heading("C.H",  text="C.H")   
+    inCourseTv.heading("Período", text="PERÍODO")
+    inCourseTv.pack()
 
-    tv4 = ttk.Treeview(aba5, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
-    tv4.column("Código",  width=60, anchor=CENTER, stretch=NO)
-    tv4.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
-    tv4.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
-    tv4.column("C.H",  width=40, anchor=CENTER, stretch=NO)
-    tv4.column("Período", width=60, anchor=CENTER, stretch=NO)
-    tv4.heading("Código",  text="CÓDIGO")
-    tv4.heading("Disciplina", text="DISCIPLINA")
-    tv4.heading("Créditos",  text="CRÉDITOS")
-    tv4.heading("C.H",  text="C.H")   
-    tv4.heading("Período", text="PERÍODO")
-    tv4.pack()
+    inCourseNumberLabel = Label(aba4, text="Você possui " + str(len(inCourseTv.get_children())) + " Disciplinas em Curso", bd=2, font=25, borderwidth=2, relief="flat")
+    inCourseNumberLabel.pack(pady=10)
 
+    pendingLabel = Label(aba5, text="Disciplinas Pendentes", bd=2, font=25, borderwidth=2, relief="flat")
+    pendingLabel.pack(pady=10)
+
+    pendingTv = ttk.Treeview(aba5, columns=("Código", "Disciplina", "Créditos", "C.H", "Período"), show=("headings"))
+    pendingTv.column("Código",  width=60, anchor=CENTER, stretch=NO)
+    pendingTv.column("Disciplina",  width=180, anchor=CENTER, stretch=NO)
+    pendingTv.column("Créditos",  width=65, anchor=CENTER, stretch=NO)
+    pendingTv.column("C.H",  width=40, anchor=CENTER, stretch=NO)
+    pendingTv.column("Período", width=60, anchor=CENTER, stretch=NO)
+    pendingTv.heading("Código",  text="CÓDIGO")
+    pendingTv.heading("Disciplina", text="DISCIPLINA")
+    pendingTv.heading("Créditos",  text="CRÉDITOS")
+    pendingTv.heading("C.H",  text="C.H")   
+    pendingTv.heading("Período", text="PERÍODO")
+    pendingTv.pack()
+
+    PendingNumberLabel = Label(aba5, text="Você possui " + str(len(pendingTv.get_children())) + " Disciplinas Pendentes", bd=2, font=25, borderwidth=2, relief="flat")
+    PendingNumberLabel.pack(pady=10)
 
     for materia in grade:
         values = materia.split(",")
-        tv.insert("", "end", values=values)
+        curriculumTv.insert("", "end", values=values)
 
 
 splash_screen.after(2000, mainWindow)
